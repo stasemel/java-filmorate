8a1e4634e0ace24731ad606e7dd583bb5b7ce94d
Stanislav Emelianov <stasemel@yandex.ru>
2025-06-03 09:15:09 +0300

    Исправления после проверки CheckStyle. SaveException

--- src/main/java/ru/yandex/practicum/filmorate/exception/SaveException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/SaveException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class SaveException extends RuntimeException{
+public class SaveException extends RuntimeException {
     public SaveException(String message) {
         super(message);
     }
